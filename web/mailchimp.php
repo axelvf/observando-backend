e1906b28c86b5a362b0ad9abc4803b21fa83010d
Axel Vasquez <axelv@squez.com.ar>
2017-08-22 19:39:36 -0300

    user-friendly variables

--- web/mailchimp.php
+++ web/mailchimp.php
@@ -1,10 +1,14 @@
 <?php
+
+$mailchimp_campaign='http://us14.campaign-archive2.com/home/?u=d914367325f70121f44ffcafb&id=1d2d0d3d76';
+$posts_limit='10';
+
 #error_reporting(E_ALL);
 header('Access-Control-Allow-Origin: *');
 
 include_once('dom_parser.php');
-$html = file_get_html('http://us14.campaign-archive2.com/home/?u=d914367325f70121f44ffcafb&id=1d2d0d3d76');
-$i = 0;
+$html = file_get_html($mailchimp_campaign);
+$i = 1;
 foreach ($html->find('li') as $element) {
     $text = $element->find('text');
     $date = trim(str_replace('-', '', $text[0]));
@@ -13,7 +17,7 @@ foreach ($html->find('li') as $element) {
         $link = $a->href;
     }
     $data[] = array("date" => "$date", "title" => "$title", "link" => "$link");
-    if (++$i > 9)
+    if (++$i > $posts_limit)
         break;
 }
 echo json_encode($data);
